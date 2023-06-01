beacf2515d3ba4a520634c29beee1b91389dbcf5
samimahlaka <samimahlaka@gmail.com>
2023-05-31 22:39:42 -0500

    Created model for Tweets app

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tweet(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.TextField(blank=True, null=True)
+    image = models.FileField(upload_to='images/' , blank=True, null=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+

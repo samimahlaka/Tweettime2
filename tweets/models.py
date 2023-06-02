0f370f8939267cac068ed5731d635a766ad5136d
samimahlaka <samimahlaka@gmail.com>
2023-06-01 21:57:54 -0500

    working on tweets views

--- tweets/models.py
+++ tweets/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Tweet(models.Model):
-    id = models.AutoField(primary_key=True)
+    #id = models.AutoField(primary_key=True)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/' , blank=True, null=True)
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #date_posted = models.DateTimeField(default=timezone.now)
+    #author = models.ForeignKey(User, on_delete=models.CASCADE)
 
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse , Http404 , JsonResponse
+from .models import Tweet
 # Create your views here.
+
+def home_view(request):
+    return HttpResponse('hello')
+
+def tweet_detail_view(request, tweet_id , *args , **kwargs):
+
+    data = {
+        'id' : tweet_id
+        }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id = tweet_id)
+        data['content'] = obj.content 
+    except:
+        data['message'] = "Not found"
+        status = 404
+    
+    return JsonResponse(data, status=status)
+    
+  
